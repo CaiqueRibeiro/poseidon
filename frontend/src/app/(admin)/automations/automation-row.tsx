dea3c0bb4c5c0467584d43466038bb0044fe0ede
Caique Ribeiro Rodrigues <ribeiro.caique95@gmail.com>
2024-08-27 19:14:28 -0300

    fix: adjusting page to edit automation

--- frontend/src/app/(admin)/automations/automation-row.tsx
+++ frontend/src/app/(admin)/automations/automation-row.tsx
@@ -40,7 +40,7 @@ export function AutomationRow(props: AutomationRowProps) {
   }
 
   async function btnEditClick() {
-    push(`/automations/new?id=${props.data.id}`);
+    push(`/automations/${props.data.id}`);
   }
 
   async function btnDeleteClick() {
